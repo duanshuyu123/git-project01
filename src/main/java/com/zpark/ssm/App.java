5d225c1ac0ddc6044c8a8f4784a4d70a2a8db749
dsy <dsy@163.com>
2023-05-06 09:10:06 +0800

    dsy master second commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! dsy master second commit!");
+        System.out.println("Hello Git! dsy hotfix first commit!");
     }
 }
